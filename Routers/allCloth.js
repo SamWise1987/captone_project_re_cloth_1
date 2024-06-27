22b2757c0692c622c13502e47bccb4b1b4e6d9fd
SamWise1987 <samuelerea23@gmail.com>
2024-06-27 18:41:39 +0200

    Commit da WebStorm con rotte aggiornate e con schema rivisti

--- Routers/allCloth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const ClothingItem = require('../models/ClothingItem');
-const router = express.Router();
-
-
-router.get('/clothing', async (req, res) => {
-    const clothing = await Clothing.find();
-    res.send(clothing);
-});
